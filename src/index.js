9c76b230188db62a81f9cfc06db46c5be249d134
George Mandis <george@mand.is>
2022-01-13 19:14:58 -0800

    Updating turboencabulator

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Turboencabulator
+ * ===
+ * Relatively self-documenting. Comment out the parts you don't need.
+ * 
+ * https://github.com/georgemandis/turboencabulator
+ */
+((e) => {
+  const c = [];
+  for (let i = 0; i < e[0].length; i += 2) c.push(String.fromCharCode(parseInt(e[0].substring(i, i + 2), 36)));
+  window[Object.getOwnPropertyNames(window).find(e => btoa(e).includes("ZXZhbA=="))](atob(c.join("")));
+})`2h1e1l392r1e1l372i2b1h372q1e2r33221d2a1d2r31223a2i2f1h2y2h2f221d2q1z1y1c2q1f212v2b2f1h342s1z302w2q1z301i2i2f2932232c371p`;
